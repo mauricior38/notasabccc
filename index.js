de470acf5e15f1650d0fe7b966bf93693cb819fd
Mauricio Corrêa <mauriciocorrea@192.168.0.38>
2025-05-06 23:21:36 -0300

    primeiro Upload

---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Notas Animal Telão
+
+app.get("/notasAnimalTelao", async (req, res) => {
+    const url="https://ws01.cavalocrioulo.org.br/wsrest/jfrmservices/evento/notasAnimalTelao";
+    const agendamento = req.query.agendamento;
+    
+    
+    try {
+    const response = await axios.get(`${url}/${agendamento}`, {
+      headers: { Accept: "application/xml" },
+      responseType: "text",
+    });
+
+    const xmlData = response.data;
+
+
+    
+    const dir = path.join(__dirname, `${agendamento}`);
+    const caminhoArquivo = path.join(dir, `notasAnimalTelao.xml`);
+
+
+    // Cria o diretório se não existir
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+
+    
+    fs.writeFileSync(caminhoArquivo, xmlData, "utf8");
+
+    res.status(200).send("XML baixado e salvo com sucesso.");
+  } catch (error) {
+    console.error("Erro ao baixar o XML:", error.message);
+    res.status(500).send("Erro ao baixar ou salvar o XML.");
+  }
+});
+
+// Notas Animal em Pista
+app.get("/animalEmPista", async (req, res) => {
+    const url = 'https://ws01.cavalocrioulo.org.br/wsrest/jfrmservices/evt/dadosAnimalPista'
+    const agendamento = req.query.agendamento;
+  
+    try {
+      const response = await axios.get(`${url}/${agendamento}`, {
+        headers: { Accept: "application/xml" },
+        responseType: "text",
+      });
+  
+      const xmlData = response.data;
+  
+  
+      
+      const dir = path.join(__dirname, `${agendamento}`);
+      const caminhoArquivo = path.join(dir, `animalEmPista.xml`);
+  
+  
+      // Cria o diretório se não existir
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      }
+  
+      
+      fs.writeFileSync(caminhoArquivo, xmlData, "utf8");
+  
+      res.status(200).send("XML baixado e salvo com sucesso.");
+    } catch (error) {
+      console.error("Erro ao baixar o XML:", error.message);
+      res.status(500).send("Erro ao baixar ou salvar o XML.");
+    }
+  });
+
+  // Ranking Femea Morfologia
+  // https://ws01.cavalocrioulo.org.br/wsrest/jfrmservices/evt/ranking/25565/FEMEA/MORFOLOGIA
+app.get("/ranking", async (req, res) => {
+    const url = 'https://ws01.cavalocrioulo.org.br/wsrest/jfrmservices/evt/ranking'
+    const sexo = req.query.sexo;
+    const prova = req.query.prova;
+    const agendamento = req.query.agendamento;
+  
+    try {
+      const response = await axios.get(`${url}/${agendamento}/${sexo}/${prova}`, {
+        headers: { Accept: "application/xml" },
+        responseType: "text",
+      });
+  
+      const xmlData = response.data;
+  
+  
+      
+      const dir = path.join(__dirname, `${agendamento}`);
+      const caminhoArquivo = path.join(dir, `RANKING-${sexo}-${prova}.xml`);
+  
+  
+      // Cria o diretório se não existir
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      }
+  
+      
+      fs.writeFileSync(caminhoArquivo, xmlData, "utf8");
+  
+      res.status(200).send("XML baixado e salvo com sucesso.");
+    } catch (error) {
+      console.error("Erro ao baixar o XML:", error.message);
+      res.status(500).send("Erro ao baixar ou salvar o XML.");
+    }
+  });
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
